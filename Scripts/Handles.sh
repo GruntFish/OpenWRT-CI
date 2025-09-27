0756f9af9f27871964a1ff19bcddeef3c5444558
GruntFish <fbyu7@qq.com>
2025-09-27 18:47:48 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PKG_PATH="$GITHUB_WORKSPACE/$WRT_DIR/package/"
+PKG_PATH="$GITHUB_WORKSPACE/$WRT_DIR/package/"
 
 #预置HomeProxy数据
 #if [ -d *"homeproxy"* ]; then
@@ -25,24 +25,24 @@
 #fi
 
 #修改qca-nss-drv启动顺序
-#NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
-#if [ -f "$NSS_DRV" ]; then
-#	echo " "
-#
-#	sed -i 's/START=.*/START=85/g' $NSS_DRV
-#
-#	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
-#fi
+NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
+if [ -f "$NSS_DRV" ]; then
+	echo " "
+
+	sed -i 's/START=.*/START=85/g' $NSS_DRV
+
+	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
+fi
 
 #修改qca-nss-pbuf启动顺序
-#NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
-#if [ -f "$NSS_PBUF" ]; then
-#	echo " "
-#
-#	sed -i 's/START=.*/START=86/g' $NSS_PBUF
-#
-#	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
-#fi
+NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
+if [ -f "$NSS_PBUF" ]; then
+	echo " "
+
+	sed -i 's/START=.*/START=86/g' $NSS_PBUF
+
+	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
+fi
 
 #修复TailScale配置文件冲突
 #TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
