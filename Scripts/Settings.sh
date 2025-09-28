d2db7f87b56ecc04e5691c7a661031398ef1fe51
GruntFish <fbyu7@qq.com>
2025-09-28 21:51:51 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #解决nss报错
-sed -i 's|3ec87f221e8905d4b6b8b3d207b7f7c4666c3bc8db7c1f06d4ae2e78f863b8f4|881cbf75efafe380b5adc91bfb1f68add5e29c9274eb950bb1e815c7a3622807|g' ./feeds/nss_packages/firmware/nss-firmware/Makefile
+#sed -i 's|3ec87f221e8905d4b6b8b3d207b7f7c4666c3bc8db7c1f06d4ae2e78f863b8f4|881cbf75efafe380b5adc91bfb1f68add5e29c9274eb950bb1e815c7a3622807|g' ./feeds/nss_packages/firmware/nss-firmware/Makefile
 		  
 . $(dirname "$(realpath "$0")")/function.sh
 #修改默认主题
